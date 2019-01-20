d7a4fa8443ea246e10cf62968ddeda2310386952
Aaron Berns <anberns@gmail.com>
2019-01-20 15:13:11 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,19 +1,31 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
+
+CREATE TABLE students
+    (id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER,
+    gpa FLOAT,
+    tardies INTEGER);
+    
 def highest_student_gpa
-  ""
+  "SELECT MAX(gpa) from students"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) from students"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) from students"
 end
 
 def total_tardies_for_all_students
+  "SELECT COUNT(tardies) from students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) from students WHERE "
 end
 
 
