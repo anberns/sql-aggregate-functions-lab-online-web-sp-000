0fdc58608438055ae7a704ccf4ee8ee553d8a874
Aaron Berns <anberns@gmail.com>
2019-01-20 15:13:47 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,12 +1,5 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
-
-CREATE TABLE students
-    (id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER,
-    gpa FLOAT,
-    tardies INTEGER);
     
 def highest_student_gpa
   "SELECT MAX(gpa) from students"
@@ -28,29 +21,3 @@ def average_gpa_for_9th_grade
   "SELECT AVG(gpa) from students WHERE grade = 9"
 end
 
-
-  describe "#highest_student_gpa" do
-    it 'finds the highest gpa' do
-      expect(@db.execute(highest_student_gpa)).to eq([[4.0]])
-    end
-  end
-  describe "#lowest_student_gpa" do
-    it 'finds the lowest gpa' do
-      expect(@db.execute(lowest_student_gpa)).to eq([[1.9]])
-    end
-  end
-  describe "#average_student_gpa" do
-    it 'finds the average gpa' do
-      expect(@db.execute(average_student_gpa)).to eq([[2.95]])
-    end
-  end
-  describe "#total_tardies_for_all_students" do
-    it 'finds the total amount of tardies for all students' do
-      expect(@db.execute(total_tardies_for_all_students)).to eq([[49]])
-    end
-  end
-  describe "#average_gpa_for_9th_grade" do
-    it 'find the average gpa for 9th grade' do
-      expect(@db.execute(average_gpa_for_9th_grade).first.first).to be_within(0.0001).of(3.3)
-    end
-  end
